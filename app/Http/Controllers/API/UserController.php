33bb9d3d9c027fca920de056ab94baf0177bc244
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-06-08 21:26:53 +0700

    fix bug

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -16,68 +16,23 @@ class UserController extends Controller
     public function register(Request $request)
     {
         try {
-            $request->validate(
-                [
+            $request->validate([
                 'name' => ['required', 'string', 'max:255'],
-                'phone' => ['required', 'string', 'max:255'],
                 'username' => ['required', 'string', 'max:255', 'unique:users'],
-                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'password' => ['required', 'string', new Password]
-                ]
-            );
+                'email' => ['required', 'email','string', 'max:255', 'unique:users'],
+                'phone' => ['nullable','string', 'max:255'],
+                'password' => ['required', 'string', new Password],
+            ]);
 
             User::create([
-                'name'=>$request->name,
-                'username'=>$request->username,
-                'phone'=>$request->phone,
-                'email'=>$request->email,
-                'password'=>Hash::make($request->password),
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'password' => Hash::make($request->password),
             ]);
 
             $user = User::where('email', $request->email)->first();
-
-            $tokenResult = $user->createToken('authToken')->plainTextToken;
-
-            return ResponseFormatter::success([
-                'access_token' => $tokenResult,
-                'token_type' => 'Bearer',
-                'user' => $user
-            ]);
-        } catch (Exception $error) {
-            return ResponseFormatter::error(
-                [
-                    'message' => 'Something went wrong',
-                    'error' => $error,
-                    
-                ], 'Authentication Failed', 500
-            );
-        }
-    }
-
-    public function login(Request $request)
-    {
-        try {
-            $request->validate([
-                'email' => 'emailRequired',
-                'password' => 'required'
-            ]);
-
-            $credentials = request(['email', 'password']);
-
-            if (!Auth::attempt($credentials)) {
-                return ResponseFormatter::error(
-                    [
-                        'message' => 'Unauthorized'
-                    ], 'Authentication Failed', 500
-                );
-            }
-
-            $user = User::where('email', $request->email)->first();
-
-            if (!Hash::check($request->password, $user->password, [])
-            ) {
-                throw new \Exception('Invalid Credentials');
-            }
 
             $tokenResult = $user->createToken('authToken')->plainTextToken;
 
@@ -86,42 +41,87 @@ class UserController extends Controller
                     'access_token' => $tokenResult,
                     'token_type' => 'Bearer',
                     'user' => $user
-                ], 'Authenticated'
+                ],
+                'User Registered'
             );
         } catch (Exception $error) {
-            return ResponseFormatter::error(
+             return ResponseFormatter::error(
                 [
-                    'message'=>'Something went wrong',
-                    'error'=>$error
-                ], 'Authentication Failed', 500
+                    'message' => 'something went wrong',
+                    'error' => $error,
+                ],
+                'Authentication Failed', 
+                500
             );
         }
     }
-    
+
+    public function login(Request $request) 
+    {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required',
+            ]);
+
+            $credentials = request(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return ResponseFormatter::error(
+                    ['message' => 'Unauthorized'],
+                    'Authentication Failed',
+                    500
+                );
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if(!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'acess_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something when wrong',
+                'error' => $error    
+            ], 'Authentication Failed', 500);
+        }
+    }
+
     public function fetch(Request $request)
     {
-        return ResponseFormatter::success(
-            $request->user(),
-            'Data profile user berhasil diambil'
-        );
+        return ResponseFormatter::success($request->user(), 'Data profile user berhasil diambil');
     }
 
     public function updateProfile(Request $request)
     {
+        $id = Auth::id();
+
+        $request->validate([
+            'name' => ['string', 'max:255'],
+            'username' => ['string', 'max:255', 'unique:users,username,'.$id],
+            'email' => ['email','string', 'max:255', 'unique:users,email,'.$id],
+            'phone' => ['nullable','string', 'max:255'],
+        ]);
+        
         $data = $request->all();
+
         $user = Auth::user();
         $user->update($data);
 
-        return ResponseFormatter::success(
-            $user, 'Profile Updated'
-        );
+        return ResponseFormatter::success($user, 'Profile updated');
     }
 
     public function logout(Request $request)
     {
         $token = $request->user()->currentAccessToken()->delete();
-        return ResponseFormatter::success(
-            $token, 'Token Revoked'
-        );
+
+        return ResponseFormatter::success($token, 'Token Revoked');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\ProductCategoryController;
 use App\Http\Controllers\API\TransactionController;
 use App\Http\Controllers\API\UserController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Models\Transaction;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,22 +14,22 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->group(function(){
-    Route::get('user', [UserController::class, 'fetch']);
-    Route::post('user', [UserController::class, 'updateProfile']);
-    Route::post('logout', [UserController::class, 'logout']);
-
-    Route::get('transaction', [TransactionController::class, 'all']);
-    Route::post('checkout', [TransactionController::class, 'checkout']);
-});
 
 Route::get('products', [ProductController::class, 'all']);
 Route::get('categories', [ProductCategoryController::class, 'all']);
 
-Route::get('register', [UserController::class, 'register']);
-Route::get('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('transactions', [TransactionController::class, 'all']);
+    Route::post('checkout', [TransactionController::class, 'checkout']);
+});
